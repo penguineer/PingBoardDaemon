5ddf7f2ffc41f9fd6c61a822433fd4dbdad5a878
Stefan Haun <mail@tuxathome.de>
2025-10-10 13:58:28 +0200

    Add local implementation of distutils.strtobool
    The strtobool function has been deprecated, so we add a local
    implementation as a replacement.

---
+++ src/util.py
@@ -0,0 +1,10 @@
+"""Utility functions."""
+
+def strtobool(val):
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return True
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return False
+    else:
+        raise ValueError(f"invalid truth value {val!r}")
